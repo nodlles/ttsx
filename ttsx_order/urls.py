45fc3914912db84d08b3bdca1b4684c8546f7f52
nodlles <lx0day@gmail.com>
2017-07-13 21:59:44 +0800

    订单页面显示

--- ttsx_order/urls.py
+++ ttsx_order/urls.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python
 # _*_ coding:utf-8 _*_
+
 from django.conf.urls import url
 
+from ttsx_order import views
+
 urlpatterns = [
-    url()
+    url(r'^order/$', views.order),
+
 ]
--- ttsx_order/views.py
+++ ttsx_order/views.py
@@ -1,3 +1,14 @@
+# coding=utf-8
 from django.shortcuts import render
 
-# Create your views here.
+from ttsx_cart.models import CartInfo
+from ttsx_user.models import UserInfo
+
+
+def order(request):
+    uid = request.session.get('uid')
+    user_obj = UserInfo.objects.get(pk=uid)
+    cart_obj = CartInfo.objects.filter(user_id=uid)
+
+    context = {'title': '提交订单', 'user_info':user_obj, 'cart_info':cart_obj}
+    return render(request, 'ttsx_order/place_order.html', context)
